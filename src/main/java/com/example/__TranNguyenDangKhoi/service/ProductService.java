78ae617ed5fbf1f773a2fd810568c5212632bd09
dominhha <dominhvan12@gmail.com>
2024-06-12 14:16:03 +0700

    initial commit

---
+++ src/main/java/com/example/__TranNguyenDangKhoi/service/ProductService.java
@@ -0,0 +1,54 @@
+package com.example.__TranNguyenDangKhoi.service;
+
+import com.example.__TranNguyenDangKhoi.model.Product;
+import com.example.__TranNguyenDangKhoi.repository.CategoryRepository;
+import com.example.__TranNguyenDangKhoi.repository.OrderDetailRepository;
+import com.example.__TranNguyenDangKhoi.repository.ProductRepository;
+import jakarta.validation.constraints.NotNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class ProductService {
+    private final ProductRepository productRepository;
+    private final OrderDetailRepository orderDetailRepository;
+    // Retrieve all products from the database
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+    // Retrieve a product by its id
+    public Optional<Product> getProductById(Long id) {
+        return productRepository.findById(id);
+    }
+    // Add a new product to the database
+    public Product addProduct(Product product) {
+        return productRepository.save(product);
+    }
+    // Update an existing product
+    public Product updateProduct(@NotNull Product product) {
+        Product existingProduct = productRepository.findById(product.getId())
+                .orElseThrow(() -> new IllegalStateException("Product with ID " +
+                        product.getId() + " does not exist."));
+        existingProduct.setName(product.getName());
+        existingProduct.setPrice(product.getPrice());
+        existingProduct.setDescription(product.getDescription());
+        existingProduct.setImageUrl(product.getImageUrl());
+        existingProduct.setCategory(product.getCategory());
+
+        return productRepository.save(existingProduct);
+    }
+    // Delete a product by its id
+    public void deleteProductById(Long id) {
+        if (!productRepository.existsById(id)) {
+            throw new IllegalStateException("Product with ID " + id + " does not exist.");
+        }
+        orderDetailRepository.deleteByProductId(id);
+        productRepository.deleteById(id);
+    }
+}
